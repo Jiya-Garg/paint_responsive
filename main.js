4eb52d74e4fa7700998b365b7ae4c1823be6c0bb
Jiya-Garg <69499533+Jiya-Garg@users.noreply.github.com>
2021-04-16 20:06:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+var last_position_of_x, last_position_of_y;
+var width = screen.width;
+var height = screen.height;
+newwidth = screen.width - 75
+newheight = screen.height - 300
+    canvas = document.getElementById('myCanvas');
+    ctx = canvas.getContext("2d");
+    
+    color = "black";
+    width_of_line = 2;
+if(width>992) {
+    document.getElementById("myCanvas").width = newwidth
+    document.getElementById("myCanvas").height = newheight
+    document.getElementById("myCanvas").style.overflow = "hidden"
+}
+    canvas.addEventListener("touchstart", mytouchstart);
+    
+    function mytouchstart(e)
+    {
+        //Addictonal Activity start
+        color = document.getElementById("colorchoice").value;
+        width_of_line = document.getElementById("linewidth").value;
+        //Addictonal Activity ends
+        last_position_of_x = e.touches[0].clientX - canvas.offsetLeft
+        last_position_of_y = e.touches[0].clientY - canvas.offsetTop
+
+    }
+
+    canvas.addEventListener("touchmove", mytouchmove);
+    function mytouchmove(e)
+    {
+
+         current_position_of_touch_x = e.touches[0].clientX - canvas.offsetLeft;
+         current_position_of_touch_y = e.touches[0].clientY - canvas.offsetTop;
+
+        
+        ctx.beginPath();
+        ctx.strokeStyle = color;
+        ctx.lineWidth = width_of_line;
+
+        console.log("Last position of x and y coordinates = ");
+        console.log("x = " + last_position_of_x + "y = " + last_position_of_y);
+        ctx.moveTo(last_position_of_x, last_position_of_y);
+
+        console.log("Current position of x and y coordinates = ");
+        console.log("x  = " + current_position_of_touch_x + "y = " + current_position_of_touch_y);
+        ctx.lineTo(current_position_of_touch_x, current_position_of_touch_y);
+        ctx.stroke();
+        
+
+        last_position_of_x = current_position_of_touch_x; 
+        last_position_of_y = current_position_of_touch_y;
+    }
+
